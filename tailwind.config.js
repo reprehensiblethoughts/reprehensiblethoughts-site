c093e8f4acd211bd20a469450f27d6aee924b19c
reprehensiblethoughts <reprehensiblethoughts@gmail.com>
2025-08-24 23:26:25 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,17 @@ export default {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/typography'),
+    // tailwind.config.js
+import typography from '@tailwindcss/typography'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {}
+  },
+  plugins: [typography]
+}
+,
   ],
 }
